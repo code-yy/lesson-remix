187cac4b3086cae46d04ddb6787e0c7cdd18ad3a
Utopia300 <jiyeyouren523@gmail.com>
2021-12-05 17:19:58 +0900

    ✨ Tutorial completed.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -62,8 +62,6 @@ export default function Index() {
     <div className="remix__page">
       <main>
         <h2>Welcome to Remix!</h2>
-        <h2>Welcome to Remix!</h2>
-        <h2>Welcome to Remix!</h2>
         <p>We're stoked that you're here. 🥳</p>
         <p>
           Feel free to take a look around the code to see how Remix does things,
